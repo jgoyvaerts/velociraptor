45241b92e3ba5d23ee823782b1e887309070dcb6
Jonathan Goyvaerts <jonathan.goyvaerts@gmail.com>
2024-07-30 02:36:48 +0200

    Update magefile.go

--- magefile.go
+++ magefile.go
@@ -270,6 +270,7 @@ func LinuxMusl386() error {
 func Linux386() error {
 	return Builder{
 		extra_tags: " release yara disable_gui ",
+		disable_cgo:   true,
 		goos:       "linux",
 		arch:       "386"}.Run()
 }
